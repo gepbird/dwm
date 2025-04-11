1af54b015d08f7d7f5011a65dd882ad45f4616be
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-04-11 18:02:11 +0200

    testing: zoom out

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p xorg.xorgserver inotify-tools
-exec Xephyr -resizeable :1 &
+exec Xephyr -resizeable -screen 1920x1080 :1 &
 while true; do
   sleep 0.1
   DISPLAY=:1 nix run || inotifywait -q -e close_write . -r
