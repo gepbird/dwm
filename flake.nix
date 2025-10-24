e17117a2949ed5f878b9d7d8a671f01c488d36e1
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-10-24 12:47:50 +0200

    flake: move package code to package.nix, use pkgs.callPackage

--- flake.nix
+++ flake.nix
@@ -13,83 +13,9 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs) fetchpatch2;
       in
       {
-        packages.default = pkgs.dwm.overrideAttrs (o: {
-          src = ./.;
-          conf = ./config.h;
-          patches = [
-            # official patches by other people
-            (fetchpatch2 {
-              name = "statusallmons";
-              url = "https://dwm.suckless.org/patches/statusallmons/dwm-statusallmons-6.2.diff";
-              hash = "sha256-rYuRi65ZzhuxLAPvIuxxB2KZmkl9Fyav+fwbclB89kQ=";
-            })
-            (fetchpatch2 {
-              name = "noborder-floatingfix";
-              url = "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff";
-              hash = "sha256-AjKKP3DJ4AFwcqwaRqzryxkVZymrW/AtUK51ELhzbQ8=";
-            })
-            ./patches/dwm-adjacenttag-6.2.diff
-            ./patches/dwm-scratchpads-20200414-728d397b.diff
-            ./patches/dwm-attachbelow-6.2.diff
-            # my official patches
-            (fetchpatch2 {
-              name = "activemonitor";
-              url = "https://dwm.suckless.org/patches/activemonitor/dwm-activemonitor-20230825-e81f17d.diff";
-              hash = "sha256-j/s3tbDZe613Fprxprti+Q/Ym4g3ddoYhOFdHX+qH7o=";
-            })
-            (fetchpatch2 {
-              name = "resizehere";
-              url = "https://dwm.suckless.org/patches/resizehere/dwm-resizehere-20230824-e81f17d.diff";
-              hash = "sha256-A+kMobZ4jD3xnEQgdeQ40SuWeimbDdNHpzLxQ/5+HAc=";
-            })
-            # my unofficial patches
-            ./patches/dwm-rofi-6.5.diff
-            ./patches/dwm-focuscursor-6.4.diff
-            ./patches/dwm-changeborder-6.4.diff
-            ./patches/dwm-noquittestmode-6.5.diff
-          ];
-          buildInputs =
-            o.buildInputs
-            ++ (with pkgs; [
-              bottom
-              clac
-              ente-auth
-              flameshot
-              gnused
-              gromit-mpx
-              lf
-              rofi
-              wrapGAppsHook
-              xfce.xfce4-terminal
-              xorg.xkill
-              zsh
-            ]);
-          postInstall = ''
-            cp chbright.sh $out/bin/chbright.sh
-            wrapProgram $out/bin/chbright.sh \
-              --prefix PATH : ${
-                with pkgs;
-                lib.makeBinPath [
-                  light
-                  dunst
-                  hck
-                ]
-              }
-            cp chvol.sh $out/bin/chvol.sh 
-            wrapProgram $out/bin/chvol.sh \
-              --prefix PATH : ${
-                with pkgs;
-                lib.makeBinPath [
-                  pulseaudio
-                  dunst
-                  ripgrep
-                ]
-              }
-          '';
-        });
+        packages.default = pkgs.callPackage ./package.nix { };
       }
     );
 }
---
+++ package.nix
@@ -0,0 +1,93 @@
+{
+  bottom,
+  clac,
+  dunst,
+  dwm,
+  ente-auth,
+  fetchpatch2,
+  flameshot,
+  gnused,
+  gromit-mpx,
+  hck,
+  lf,
+  lib,
+  light,
+  pulseaudio,
+  ripgrep,
+  rofi,
+  wrapGAppsHook,
+  xfce,
+  xorg,
+  zsh,
+}:
+
+dwm.overrideAttrs (o: {
+  src = ./.;
+  conf = ./config.h;
+  patches = [
+    # official patches by other people
+    (fetchpatch2 {
+      name = "statusallmons";
+      url = "https://dwm.suckless.org/patches/statusallmons/dwm-statusallmons-6.2.diff";
+      hash = "sha256-rYuRi65ZzhuxLAPvIuxxB2KZmkl9Fyav+fwbclB89kQ=";
+    })
+    (fetchpatch2 {
+      name = "noborder-floatingfix";
+      url = "https://dwm.suckless.org/patches/noborder/dwm-noborderfloatingfix-6.2.diff";
+      hash = "sha256-AjKKP3DJ4AFwcqwaRqzryxkVZymrW/AtUK51ELhzbQ8=";
+    })
+    ./patches/dwm-adjacenttag-6.2.diff
+    ./patches/dwm-scratchpads-20200414-728d397b.diff
+    ./patches/dwm-attachbelow-6.2.diff
+    # my official patches
+    (fetchpatch2 {
+      name = "activemonitor";
+      url = "https://dwm.suckless.org/patches/activemonitor/dwm-activemonitor-20230825-e81f17d.diff";
+      hash = "sha256-j/s3tbDZe613Fprxprti+Q/Ym4g3ddoYhOFdHX+qH7o=";
+    })
+    (fetchpatch2 {
+      name = "resizehere";
+      url = "https://dwm.suckless.org/patches/resizehere/dwm-resizehere-20230824-e81f17d.diff";
+      hash = "sha256-A+kMobZ4jD3xnEQgdeQ40SuWeimbDdNHpzLxQ/5+HAc=";
+    })
+    # my unofficial patches
+    ./patches/dwm-rofi-6.5.diff
+    ./patches/dwm-focuscursor-6.4.diff
+    ./patches/dwm-changeborder-6.4.diff
+    ./patches/dwm-noquittestmode-6.5.diff
+  ];
+  buildInputs = o.buildInputs ++ [
+    bottom
+    clac
+    ente-auth
+    flameshot
+    gnused
+    gromit-mpx
+    lf
+    rofi
+    wrapGAppsHook
+    xfce.xfce4-terminal
+    xorg.xkill
+    zsh
+  ];
+  postInstall = ''
+    cp chbright.sh $out/bin/chbright.sh
+    wrapProgram $out/bin/chbright.sh \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          light
+          dunst
+          hck
+        ]
+      }
+    cp chvol.sh $out/bin/chvol.sh 
+    wrapProgram $out/bin/chvol.sh \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          pulseaudio
+          dunst
+          ripgrep
+        ]
+      }
+  '';
+})
