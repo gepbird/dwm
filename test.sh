5f39db4ed0b2f20c5d3f21e0b2cba236b679f24f
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-03-01 14:37:56 +0100

    test.sh: fix xorg namespace deprecation warning

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p xorg.xorgserver inotify-tools
+#!nix-shell -i bash -p xorgserver inotify-tools
 export DWM_TEST=1
 exec Xephyr -resizeable -screen 1920x1080 :1 &
 while true; do
