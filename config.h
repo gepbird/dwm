a791ffd601e35831634ef692b104b6d3173bd08f
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-05-23 15:37:45 +0200

    dwm: add refreshrate to fix build after 6.7

--- config.h
+++ config.h
@@ -62,6 +62,7 @@ static const float mfact        = 0.55; /* factor of master area size [0.05..0.9
 static const int nmaster        = 1;    /* number of clients in master area */
 static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
+static const int refreshrate    = 120;  /* refresh rate (per second) for client move/resize */
 static const int attachbelow    = 1;    /* 1 means attach after the currently active window */
 
 static const Layout layouts[] = {
